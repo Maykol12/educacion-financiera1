6be7dbc7b9cad134aecd5551661fa39f5e927b64
Maykol12 <122546807+Maykol12@users.noreply.github.com>
2026-01-30 13:37:25 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+from setup_db import crear_base_datos # Importamos tu script de creación
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    crear_base_datos() # Esto creará las tablas automáticamente al iniciar
+    app.run()
